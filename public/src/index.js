ae5e5dd58926131b1d7f8f954acc8827cd10ae99
samyak619 <samyakhadse@gmail.com>
2024-04-12 19:27:44 +0530

    Add @fvilers/disable-react-devtools package to dependencies and disable React DevTools in production environment

--- public/src/index.js
+++ public/src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import {disableReactDevTools} from "@fvilers/disable-react-devtools";
 
 // CSS Styles
 import "./index.css";
+
+if(process.env.NODE_ENV === "production"){
+  disableReactDevTools();
+}   
 
 createRoot(document.getElementById("root")).render(<App />);
